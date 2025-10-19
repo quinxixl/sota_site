d996a0151e0aec15f05a19990ae3718b0dda592b
quinxixl <godfothisworld@quinxixl.local>
2025-10-19 12:13:50 +0300

    hero page changes

--- sota_front/src/animations/Lenis.js
+++ sota_front/src/animations/Lenis.js
@@ -2,11 +2,11 @@ import React from "react";
 import lenis from "lenis"
 
 function Lenis({children}) {
-
-
     return (
         <div className="lenis">
             {children}
         </div>
     )
 }
+
+export default Lenis
