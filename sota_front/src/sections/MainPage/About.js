a48530af25d13942a7d4bfd33a0db366d13a086a
Артём <dhdb82@inbox.ru>
2025-10-25 09:45:00 +0300

    about-2

--- sota_front/src/sections/MainPage/About.js
+++ sota_front/src/sections/MainPage/About.js
@@ -1,8 +1,25 @@
-import React, {useRef, useEffect} from "react";
+import React, {useEffect} from "react";
 
 function About () {
     return (
         <div className='about'>
+            <p className="about__title">
+                О нас
+            </p>
+            <div className="about__container-subtitle">
+                <p className="about__subtitle">
+                    Ваш бизнес — это улей.
+                </p>
+                <p className="about__subtitle">
+                    Мы — его защита и опора.
+                </p>
+            </div>
+            <p className="about__subtitle-main">
+                Мы действуем <br/>
+                как сплоченный рой, <br/>
+                где каждый специалист <br/>
+                точно знает свою задачу. <br/>
+            </p>
         </div>
     )
 }
