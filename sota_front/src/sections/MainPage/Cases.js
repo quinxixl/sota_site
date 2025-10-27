7dd64d0b8b2a05b9f1fc3d4e6d37c29cd4180cc7
Никита <nikita@MacBook-Pro-Nikita.local>
2025-10-27 16:33:20 +0300

    cases update

--- sota_front/src/sections/MainPage/Cases.js
+++ sota_front/src/sections/MainPage/Cases.js
@@ -1,22 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import request from "../../scripts/request";
 import cta from "../../icons/services_cta.svg";
 import sotaSvg from "../../icons/sota.svg";
 
 function CasesPage(){
+    const imageRef = useRef(null);
     const [requests, setRequests] = useState([]);
+
     useEffect(() => {
         const url = 'http://localhost:3000/api/cases/';
             request(url, setRequests)
     }, [])
+
+    const noScroll = () => {
+        document.body.style.overflow = 'hidden';
+    }
+
+    const scroll = () => {
+        document.body.style.overflow = 'auto';
+
+    }
+
+
+
     return (
         <div className="cases">
             <p className="cases__title">Кейсы</p>
             <div className="cases__container">
                 {requests.map((item, index) => (
-                    <div key={index} className="cases__item">
+                    <div key={index} className="cases__item" onMouseEnter={noScroll} onMouseLeave={scroll}>
                         <img src={sotaSvg} alt="" className="cases__item-sota"/>
-                        <img src={`${item.design_url}`} alt="" className="cases__item-design"/>
+                        <div className="cases__item-design">
+                            <div className='cases__item-design-imgwrap'>
+                                <img src={`${item.design_url}`} alt=""/>
+                            </div>
+                        </div>
                         <img src={`${item.logo_url}`} alt="" className="cases__item-logo"/>
                     </div>
                 ))}
