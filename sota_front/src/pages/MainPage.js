a22a13f40577393ab97fa12598576134980687f1
Никита <nikita@MacBook-Pro-Nikita.local>
2025-10-29 11:56:41 +0300

    --

--- sota_front/src/pages/MainPage.js
+++ sota_front/src/pages/MainPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../sections/MainPage/Hero";
 import Services from "../sections/MainPage/Services";
 import Cases from "../sections/MainPage/Cases";
-import Footer from "../components/UI/Footer";
 import About from "../sections/MainPage/About";
 
 function MainPage() {
